832549d50c254beb86b399c7030d2d21df53cdf5
volt <vladimir305200@mail.ru>
2021-06-01 23:09:27 +0500

    Функции возвращающие случайные числа, ошибки в коде учтены

--- js/main.js
+++ js/main.js
@@ -2,26 +2,32 @@
 // Взято с https://learn.javascript.ru
 // "Ошибку" добавил от себя)
 
-const RANDOM_INTEGER = function (min, max) {
-    let rand = min + Math.random() * (max + 1 - min);
+const getRandomInteger = function (min, max) {
     if (min >= max) {
-       return 'ошибка'; 
-    }
+      throw new RangeError('Значение min не должно быть больше max');
+     }
+     if (min < 0 || max < 0) {
+      throw new RangeError('Нужны только положительные числа');  
+     }
+    let rand = min + Math.random() * (max + 1 - min);    
     return Math.floor(rand);
   }
   
- console.log(RANDOM_INTEGER(1,5)); 
-
-
-// Случайное число с плавающей точкой
-// Попробовал написать сам, знаю что написал не особо правильно, но вроде работает
-
-function randomFractNumber(min, max, _afterDot){
-  let rand = min + Math.random() * (max + 1 - min);
-  if (min >= max) {
-    return 'ошибка'; 
- }
-  return (rand.toFixed(_afterDot));
-}
-
-console.log(randomFractNumber(1,5,2));
+  console.log(getRandomInteger(1,5)); 
+  
+  
+  // Случайное число с плавающей точкой
+  // Попробовал написать сам, знаю что написал не особо правильно, но вроде работает
+  
+  const getRandomFractNumber = function (min, max, _afterDot){
+    if (min >= max) {
+      throw new RangeError('Значение min не должно быть больше max'); 
+    }
+    if (min < 0 || max < 0) {
+      throw new RangeError('Нужны только положительные числа');  
+    }  
+    let rand = min + Math.random() * (max + 1 - min);
+    return (rand.toFixed(_afterDot));
+  }
+  
+  console.log(getRandomFractNumber(1,5,5));
