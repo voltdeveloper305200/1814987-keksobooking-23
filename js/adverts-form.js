087e82f924476ca34c81208c9ba1f0b1fc922863
volt <vladimir305200@mail.ru>
2021-06-30 17:03:02 +0500

    Добавлена синхронизация времени выезда и заезда, а также доделана валидация минимальной цены

--- js/adverts-form.js
+++ js/adverts-form.js
@@ -1,13 +1,12 @@
-import {HouseType} from './constants.js';
-
 const MAX_INPUT_PRICE = 1000000;
 const MinPriceLimitMap = {
   'bungalow': 0,
   'flat': 1000,
   'hotel': 3000,
   'house': 5000,
-  'palace': 1000,
+  'palace': 10000,
 };
+
 const RoomsQuantity = {
   ONE: '1',
   TWO: '2',
@@ -28,8 +27,8 @@ const inputPrice = adForm.querySelector('#price');
 const typeSelect = adForm.querySelector('#type');
 const roomsSelect = adForm.querySelector('#room_number');
 const placesSelect = adForm.querySelector('#capacity');
-
-// Функция, делающая форму неактивной
+const timeInSelect = adForm.querySelector('#timein');
+const timeOutSelect = adForm.querySelector('#timeout');
 
 const deactivate = () =>{
   adForm.classList.add('ad-form--disabled');
@@ -38,8 +37,6 @@ const deactivate = () =>{
   });
 };
 
-// Функция, делающая форму активной
-
 const activate = () =>{
   adForm.classList.remove('ad-form--disabled');
   adFormFieldsets.forEach((element) => {
@@ -47,65 +44,59 @@ const activate = () =>{
   });
 };
 
-// Валидация формы
-
-// Заголовок
 inputTitle.addEventListener('input', () =>{
   const valueLength = inputTitle.value.length;
+
   if (valueLength < 30) {
     inputTitle.setCustomValidity(`Ещё ${  30 - valueLength } симв.`);
-  } else if(valueLength > 100){
+  } else if (valueLength > 100) {
     inputTitle.setCustomValidity(`Удалите лишние ${  valueLength - 100 } симв.`);
-  } else{
+  } else {
     inputTitle.setCustomValidity('');
   }
 });
 
-// Цена
-inputPrice.addEventListener('input',() =>{
+const validatePrice = () => {
+  const minPrice = MinPriceLimitMap[typeSelect.value];
+  let error = '';
   const valuePrice = Number(inputPrice.value);
-  let err = '';
+  inputPrice.placeholder = minPrice;
+
   if (valuePrice >= MAX_INPUT_PRICE) {
-    err = 'Цена слишком высокая';
-  } else if(valuePrice < 0){
-    err = 'Допустимы только положительные числа';
+    error = 'Цена слишком высокая';
+  }else if (valuePrice < 0) {
+    error = 'Допустимы только положительные числа';
+  }else if (valuePrice < minPrice) {
+    error = 'Цена меньше минимальной';
   }
-  inputPrice.setCustomValidity(err);
-});
-
-// Тип жилья
-const setPricePlaceholder = (evt) => {
-  if (evt.target.value === HouseType.FLAT) {
-    inputPrice.placeholder = MinPriceLimitMap.flat;
-  }else if(evt.target.value ===  HouseType.BUNGALOW){
-    inputPrice.placeholder = MinPriceLimitMap.bungalow;
-  }else if(evt.target.value ===  HouseType.HOTEL){
-    inputPrice.placeholder = MinPriceLimitMap.hotel;
-  }else if(evt.target.value ===  HouseType.HOUSE){
-    inputPrice.placeholder = MinPriceLimitMap.house;
-  }else if(evt.target.value ===  HouseType.PALACE){
-    inputPrice.placeholder = MinPriceLimitMap.palace;
-  }
+  inputPrice.setCustomValidity(error);
 };
-
-typeSelect.addEventListener('change', setPricePlaceholder);
-
 
 const validateCapacity = () =>{
   let error = '';
   if (roomsSelect.value === RoomsQuantity.ONE && placesSelect.value !== PlacesQuantity.ONE) {
     error = '1 комната только для 1 гостя';
-  }else if(roomsSelect.value === RoomsQuantity.TWO && (placesSelect.value === PlacesQuantity.THREE
+  }else if (roomsSelect.value === RoomsQuantity.TWO && (placesSelect.value === PlacesQuantity.THREE
     || placesSelect.value === PlacesQuantity.ZERO)){
     error = '2 комнаты для 1го или 2х гостей';
-  }else if(roomsSelect.value === RoomsQuantity.THREE && placesSelect.value === PlacesQuantity.ZERO){
+  }else if (roomsSelect.value === RoomsQuantity.THREE && placesSelect.value === PlacesQuantity.ZERO){
     error = '3 комнаты для 1го, 2х  или 3х гостей';
-  }else if(roomsSelect.value === RoomsQuantity.HUNDRED && placesSelect.value !== PlacesQuantity.ZERO){
+  }else if (roomsSelect.value === RoomsQuantity.HUNDRED && placesSelect.value !== PlacesQuantity.ZERO){
     error = '100 комнат не для гостей';
   }
   placesSelect.setCustomValidity(error);
 };
 
+timeInSelect.addEventListener('change', (evt) => {
+  timeOutSelect.value = evt.target.value;
+});
+
+timeOutSelect.addEventListener('change', (evt) => {
+  timeInSelect.value = evt.target.value;
+});
+
+inputPrice.addEventListener('input', validatePrice);
+typeSelect.addEventListener('change', validatePrice);
 roomsSelect.addEventListener('change', validateCapacity);
 placesSelect.addEventListener('change', validateCapacity);
 
